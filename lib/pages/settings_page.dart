59cf0514d74fdfb2fa83d40ab32bb8c88a5c3ad1
LeBern Matthews <128014055+LeBern-Matthews@users.noreply.github.com>
2026-03-11 19:33:45 -0400

    Revert "Map trial"

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import '../components/country_selector.dart'; // Add this import
 import 'package:provider/provider.dart';
-import '../components/themeButton.dart';
 import '../themes/theme_provider.dart';
 import '../themes/light_mode.dart';
 import '../themes/dark_mode.dart';
 import '../services/country_provider.dart';
 //import '../services/determine_country_info.dart';
-import '../components/appbar.dart'; // code for custom AppBar
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -17,8 +15,8 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  //final String _selectedCountry = "Select a country";
-  String _selectedTheme = "Change the theme of your app.";
+ //final String _selectedCountry = "Select a country";
+  String _selectedTheme ="Change the app theme";
 
   final ExpansibleController _themeController = ExpansibleController();
   final ExpansibleController _countryController = ExpansibleController();
@@ -26,89 +24,66 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: "Settings"),
+      appBar: AppBar(
+        title: const Text("Settings"),
+        centerTitle: false,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 24, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 30),
-              const Text(
-                "Theme",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Text(_selectedTheme, style: TextStyle(fontSize: 14 ),),
-              SizedBox(height: 20),
-              Row(
-                children: [
-
-                  ThemeButton(        // Light theme button
-                    label: 'Light',
-                    icon: Icons.light_mode,
-                    onPressed: () {
-                      setState(() {
-                        _selectedTheme = 'Light';
-                      });
-                      Provider.of<ThemeProvider>(
-                        context,
-                        listen: false,
-                      ).setTheme(lightmode);
-                      _themeController.collapse();
-                    },
-                  ),
-
-                  const SizedBox(width: 10),
-
-                  ThemeButton(       // Dark theme button
-                    label: 'Dark',
-                    icon: Icons.dark_mode,
-                    onPressed: () {
-                      setState(() {
-                        _selectedTheme = 'Dark';
-                      });
-                      Provider.of<ThemeProvider>(
-                        context,
-                        listen: false,
-                      ).setTheme(darkmode);
-                      _themeController.collapse();
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Divider(
-                color: const Color.fromARGB(255, 188, 177, 177),
-                thickness: 1.5,
-              ),
-
-              SizedBox(height: 30),
-              ExpansionTile(
-                collapsedBackgroundColor: Colors.red[100],
-                controller: _countryController,
-                title: const Text("Country"),
-                subtitle: Text(context.watch<CountryProvider>().country),
-                trailing: const Icon(Icons.arrow_drop_down_rounded),
-                children: countryOptions().map((country) {
-                  return ListTile(
-                    title: Text(country),
-                    onTap: () async {
-                      Provider.of<CountryProvider>(
-                        context,
-                        listen: false,
-                      ).setCountry(country);
-                      await Provider.of<CountryProvider>(
-                        context,
-                        listen: false,
-                      ).loadJsonData(country);
-                      _countryController.collapse();
-                    },
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            ExpansionTile(
+              controller: _themeController,
+              title: const Text("Theme"),
+              subtitle:  Text(_selectedTheme),
+              trailing: const Icon(Icons.arrow_drop_down_rounded),
+              children: [
+                ListTile(
+                  title: const Text("Light"),
+                  leading: const Icon(Icons.light_mode),
+                  onTap: () {
+                    setState(() {
+                      _selectedTheme = "Light";
+                    });
+                    Provider.of<ThemeProvider>(context, listen: false).setTheme(lightmode);
+                    _themeController.collapse();
+                  },
+                ),
+                ListTile(
+                  title: const Text("Dark"),
+                  leading: const Icon(Icons.dark_mode),
+                  onTap: () {
+                    setState(() {
+                      _selectedTheme = "Dark";
+                    });
+                    Provider.of<ThemeProvider>(context, listen: false).setTheme(darkmode);
+                    _themeController.collapse();
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            ExpansionTile(
+              controller: _countryController,
+              title: const Text("Country"),
+              subtitle: Text(context.watch<CountryProvider>().country),
+              trailing: const Icon(Icons.arrow_drop_down_rounded),
+              children: countryOptions().map((country) {
+                return ListTile(
+                  title: Text(country),
+                  onTap: () async {
+                    Provider.of<CountryProvider>(context, listen: false).setCountry(country);
+                    await Provider.of<CountryProvider>(context, listen: false).loadJsonData(country);
+                    _countryController.collapse();
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+          
         ),
+        
       ),
     );
   }
